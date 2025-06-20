edd817dcbed9c6e4e9c479b30343778f0a0904c7
kychiu <kychiu@polyu.edu.hk>
2025-06-20 11:44:45 +0800

    feat: update

---
+++ src/components/Plates/LineChartPlate.tsx
@@ -0,0 +1,40 @@
+import { Gauge, gaugeClasses } from "@mui/x-charts/Gauge";
+import React from "react";
+
+type LineChartPlateProps = {
+  title: string;
+  value: number;
+  text: string;
+  color: string;
+  icon?: any;
+};
+
+export const LineChartPlate: React.FC<LineChartPlateProps> = (props) => {
+  const { title, value, text, color, icon } = props;
+  return (
+    <div className="flex flex-col items-center rounded-xl border-[2px] border-[#ccc]">
+      <div className="relative w-full flex justify-between">
+        <div className="w-full">{title}</div>
+        {icon && <div className="absolute top-0 right-0">{icon}</div>}
+      </div>
+      <div className="w-full h-[75px]">
+        <Gauge
+          text={text}
+          value={value}
+          startAngle={-110}
+          endAngle={110}
+          innerRadius="80%"
+          outerRadius="100%"
+          sx={{
+            [`& .${gaugeClasses.valueText}`]: {
+              fontSize: 12,
+            },
+            [`& .${gaugeClasses.valueArc}`]: {
+              fill: color,
+            },
+          }}
+        />
+      </div>
+    </div>
+  );
+};
--- src/components/Plates/index.ts
+++ src/components/Plates/index.ts
@@ -1,1 +1,2 @@
 export * from "./GaugePlate";
+export * from "./LineChartPlate";
