025567ab19913aa54221a4e105c224929a4f4729
kychiu <kychiu@polyu.edu.hk>
2025-06-20 16:33:55 +0800

    feat: update

--- src/components/Control/Statistics.tsx
+++ src/components/Control/Statistics.tsx
@@ -1,15 +1,27 @@
 import ElectricBoltIcon from "@mui/icons-material/ElectricBolt";
 import ElectricalServicesIcon from "@mui/icons-material/ElectricalServices";
-import ThermostatIcon from "@mui/icons-material/Thermostat";
 import EqualizerIcon from "@mui/icons-material/Equalizer";
+import ThermostatIcon from "@mui/icons-material/Thermostat";
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
 import { LANG, LANG_OBJ } from "../../utils";
 import { GaugePlate, LineChartPlate } from "../Plates";
-import dayjs from "dayjs";
 import { ChargePlate } from "../Plates/ChargePlate";
-import { useState } from "react";
+
+const defaultStats = {
+  ambientTemp: 21.25,
+  stationTemp: 32.5,
+  stationAmp: 8.53,
+  isConencted: true,
+};
 
 export const Statistics: React.FC = () => {
-  const [isConnected, setIsConnected] = useState(true);
+  const [stats, setStats] = useState(defaultStats);
+
+  useEffect(() => {
+    setStats(defaultStats);
+  }, []);
+
   const [ambientTemps, setAmbientTemps] = useState([]);
   const [stationTemps, setStationTemps] = useState([]);
 
@@ -18,8 +30,8 @@ export const Statistics: React.FC = () => {
       <div className="row-start-1 col-span-1 row-span-1">
         <GaugePlate
           title={LANG(LANG_OBJ.GAUGE.AMBIENT_TEMP)}
-          text={"21.25°C"}
-          value={21.25}
+          text={`${stats.ambientTemp} °C`}
+          value={stats.ambientTemp}
           color={"#4c84ff"}
           icon={<ThermostatIcon sx={{ color: "#4c84ff" }} />}
         />
@@ -27,8 +39,8 @@ export const Statistics: React.FC = () => {
       <div className="row-start-2 col-span-1 row-span-1">
         <GaugePlate
           title={LANG(LANG_OBJ.GAUGE.STATION_TEMP)}
-          text={"23.5°C"}
-          value={23.5}
+          text={`${stats.stationTemp} °C`}
+          value={stats.stationTemp}
           color={"#52b202"}
           icon={<ThermostatIcon sx={{ color: "#52b202" }} />}
         />
@@ -36,8 +48,8 @@ export const Statistics: React.FC = () => {
       <div className="row-start-3 col-span-1 row-span-1">
         <GaugePlate
           title={LANG(LANG_OBJ.GAUGE.CURRENT)}
-          text={"8.5A"}
-          value={8.5}
+          text={`${stats.stationAmp} Amp`}
+          value={stats.stationAmp}
           valueMax={13}
           color={"#ffa500"}
           icon={<ElectricBoltIcon sx={{ color: "#ffa500" }} />}
@@ -46,10 +58,10 @@ export const Statistics: React.FC = () => {
       <div className="row-start-4 col-span-1 row-span-1">
         <ChargePlate
           title={LANG(
-            isConnected ? LANG_OBJ.CHARGING.ON : LANG_OBJ.CHARGING.OFF
+            stats.isConencted ? LANG_OBJ.CHARGING.ON : LANG_OBJ.CHARGING.OFF
           )}
           icon={<ElectricalServicesIcon />}
-          isConnected={isConnected}
+          isConnected={stats.isConencted}
         />
       </div>
       <div className="col-span-2 row-span-2">
