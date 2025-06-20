0bc6e0f6a79477faace208cc4b909ee30140c4c9
kychiu <kychiu@polyu.edu.hk>
2025-06-20 10:39:57 +0800

    feat: update

--- src/components/Control/Statistics.tsx
+++ src/components/Control/Statistics.tsx
@@ -1,7 +1,15 @@
+import { Gauge } from "@mui/x-charts/Gauge";
+
 export const Statistics: React.FC = () => {
   return (
     <div className="">
-      <img src="/images/demo.png" />
+      <Gauge
+        value={75}
+        startAngle={0}
+        endAngle={360}
+        innerRadius="80%"
+        outerRadius="100%"
+      />
     </div>
   );
 };
