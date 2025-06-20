ee7101559d7c83981d8794a729d422bf280ae647
kychiu <kychiu@polyu.edu.hk>
2025-06-20 10:25:15 +0800

    feat: update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,15 +4,25 @@ export const Header: React.FC = () => {
   const [lang, setLang] = useState(sessionStorage.getItem("LANGUAGE"));
 
   const handleSetLang = (lang: LANG_ENUM) => {
-    if (lang === LANG_ENUM.ZH_S) return;
+    if (lang === LANG_ENUM.JP) return;
     setLang(lang);
     sessionStorage.setItem("LANGUAGE", lang);
     window.location.reload();
   };
 
   return (
-    <div className="flex justify-between items-center py-4 px-4 w-full h-24 bg-(--color-secondary)">
+    <div className="flex justify-between items-center py-4 px-4 sm:px-24 w-full h-24 bg-(--color-secondary)">
       <img className="h-full object-contain" src="/images/logo.png" />
+      <div className="flex flex-col items-center">
+        <span>
+          ポータブル電動工具充電ステーション IoT 温度および電流監視システム
+        </span>
+        <span>
+          Electrical Hand Tools Charging Station loT Temperature and Current
+          Monitoring & Control System
+        </span>
+        <span>手提電工具充電站loT溫度及電流監控系統</span>
+      </div>
       <div className="flex gap-2">
         {Object.values(LANG_ENUM).map((e) => {
           return (
@@ -23,8 +33,10 @@ export const Header: React.FC = () => {
               }}
               className="cursor-pointer"
             >
-              {e == LANG_ENUM.ZH_S ? (
-                <div className="cursor-not-allowed text-[#aaa]">{LANG_OBJ.LANG[e]}</div>
+              {e == LANG_ENUM.JP ? (
+                <div className="cursor-not-allowed text-[#aaa]">
+                  {LANG_OBJ.LANG[e]}
+                </div>
               ) : e == lang ? (
                 <b className="underline">{LANG_OBJ.LANG[e]}</b>
               ) : (
--- src/utils/lang.ts
+++ src/utils/lang.ts
@@ -1,78 +1,78 @@
 export enum LANG_ENUM {
   EN = "EN",
-  ZH_S = "ZH_S",
+  JP = "JP",
   ZH_T = "ZH_T",
 }
 
 export const LANG_OBJ = {
   LANG: {
     EN: "EN",
-    ZH_S: "簡",
+    JP: "日",
     ZH_T: "繁",
   },
   SYSTEM_TITLE: {
     EN: "HSWW SYSTEM",
-    ZH_S: "",
+    JP: "",
     ZH_T: "暑熱天氣警報系統",
   },
   HSWW_AMBER_OUTDOOR: {
     EN: "ISSUE AMBER WARNING (OUTDOOR)",
-    ZH_S: "",
+    JP: "",
     ZH_T: "發出黃色警報 (戶外)",
   },
   HSWW_RED_OUTDOOR: {
     EN: "ISSUE RED WARNING (OUTDOOR)",
-    ZH_S: "",
+    JP: "",
     ZH_T: "發出紅色警報 (戶外)",
   },
   HSWW_BLACK_OUTDOOR: {
     EN: "ISSUE BLACK WARNING (OUTDOOR)",
-    ZH_S: "",
+    JP: "",
     ZH_T: "發出黑色警報 (戶外)",
   },
   HSWW_CANCEL_OUTDOOR: {
     EN: "CANCEL WARNING",
-    ZH_S: "",
+    JP: "",
     ZH_T: "取消警報",
   },
   HSWW_AMBER_INDOOR: {
     EN: "ISSUE AMBER WARNING (INDOOR)",
-    ZH_S: "",
+    JP: "",
     ZH_T: "發出黃色警報 (室内)",
   },
   HSWW_RED_INDOOR: {
     EN: "ISSUE RED WARNING (INDOOR)",
-    ZH_S: "",
+    JP: "",
     ZH_T: "發出紅色警報 (室内)",
   },
   HSWW_BLACK_INDOOR: {
     EN: "ISSUE BLACK WARNING (INDOOR)",
-    ZH_S: "",
+    JP: "",
     ZH_T: "發出黑色警報 (室内)",
   },
   HSWW_CANCEL_INDOOR: {
     EN: "CANCEL WARNING",
-    ZH_S: "",
+    JP: "",
     ZH_T: "取消警報",
   },
   SYSTEM_MODE: {
     AUTO: {
       EN: "AUTO MODE",
-      ZH_S: "",
+      JP: "",
       ZH_T: "自動模式",
     },
     MANUAL: {
       EN: "MANUAL MODE",
-      ZH_S: "",
+      JP: "",
       ZH_T: "手動模式",
     },
   },
-  CURRENT_SITUATION_TEXT: { EN: "", ZH_S: "", ZH_T: "目前暑熱天氣警報" },
+  CURRENT_SITUATION_TEXT: { EN: "", JP: "", ZH_T: "目前暑熱天氣警報" },
   WARNING_COLOR: {
-    AMBER: { EN: "AMBER", ZH_S: "", ZH_T: "黃" },
-    RED: { EN: "RED", ZH_S: "", ZH_T: "紅" },
-    BLACK: { EN: "BLACK", ZH_S: "", ZH_T: "黑" },
-    CANCEL: { EN: "CANCEL", ZH_S: "", ZH_T: "取消" },
+    AMBER: { EN: "AMBER", JP: "", ZH_T: "黃" },
+    RED: { EN: "RED", JP: "", ZH_T: "紅" },
+    BLACK: { EN: "BLACK", JP: "", ZH_T: "黑" },
+    CANCEL: { EN: "CANCEL", JP: "", ZH_T: "取消" },
   },
 };
 
