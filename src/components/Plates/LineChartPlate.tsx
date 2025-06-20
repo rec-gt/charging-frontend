ebf1de20770ce265288424a0767e73ba9ad2e14b
kychiu <kychiu@polyu.edu.hk>
2025-06-20 12:18:51 +0800

    feat: update

--- src/components/Plates/LineChartPlate.tsx
+++ src/components/Plates/LineChartPlate.tsx
@@ -14,7 +14,7 @@ export const LineChartPlate: React.FC<LineChartPlateProps> = (props) => {
   return (
     <div className="w-full h-full flex flex-col items-center rounded-xl border-[2px] border-[#ccc]">
       <div className="relative w-full flex justify-between">
-        <div className="w-full">{title}</div>
+        <div className="px-4 w-full text-center">{title}</div>
         {icon && <div className="absolute top-0 right-0">{icon}</div>}
       </div>
       <div className="w-full h-full">
