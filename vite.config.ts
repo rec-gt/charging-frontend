6ca64c7c4f7f7dc41a089fefbe5efaef49d9eda6
kychiu <kychiu@polyu.edu.hk>
2025-06-24 16:04:55 +0800

    feat: update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // server: {
+  //   port: 80,
+  //   // host: true,
+  // },
   server: {
     port: 80,
     host: true,
