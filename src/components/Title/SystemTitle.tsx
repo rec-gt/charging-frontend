c60291f8e90ad32851505d2fffe8d17c58983502
kychiu <kychiu@polyu.edu.hk>
2025-06-24 15:28:29 +0800

    feat: update

--- src/components/Title/SystemTitle.tsx
+++ src/components/Title/SystemTitle.tsx
@@ -3,10 +3,10 @@ import { LANG, LANG_ENUM, LANG_OBJ } from "../../utils";
 export const SystemTitle: React.FC = () => {
   return (
     <div className="flex flex-col items-center text-center mt-2 mb-4 font-bold">
+      <div className="text-[16pt]!">{LANG(LANG_OBJ.TITLE)}</div>
       {sessionStorage.getItem("LANGUAGE") !== LANG_ENUM.JP && (
-        <div className="text-[16pt]! mt-2">{LANG(LANG_OBJ.TITLE)}</div>
+        <div className="text-[14pt]! mt-2">{LANG_OBJ.TITLE.JP}</div>
       )}
-      <div className="text-[18pt]!">{LANG_OBJ.TITLE.JP}</div>
     </div>
   );
 };
