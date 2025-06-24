9d7ee656d4a6f46cd94f38626a7db3ffb9c47060
kychiu <kychiu@polyu.edu.hk>
2025-06-24 16:50:36 +0800

    feat: update

--- src/components/SettingPage/SettingPage.tsx
+++ src/components/SettingPage/SettingPage.tsx
@@ -4,7 +4,9 @@ export const SettingPage: React.FC = () => {
   return (
     <PageLayout>
       <div className="flex flex-col gap-2 justify-between sm:flex-row mb-10 min-h-[500px]">
-        <div className="flex-1"></div>
+        <div className="flex-1">
+          SET POINT TEMPERATURE <input />
+        </div>
       </div>
     </PageLayout>
   );
