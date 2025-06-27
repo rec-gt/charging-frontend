47223a6aada1079345aa37e8e01052be105b4073
kychiu <kychiu@polyu.edu.hk>
2025-06-27 17:25:17 +0800

    feat: update

--- src/components/Plates/Plate.tsx
+++ src/components/Plates/Plate.tsx
@@ -12,11 +12,11 @@ export const Plate: React.FC<PlateProps> = (props) => {
   return (
     <div className="w-full h-full flex flex-col items-center rounded-xl border-[2px] border-[#ccc]">
       <div className="relative w-full flex justify-between">
-        <div className="w-[32px] px-1 sm:w-full flex justify-start items-center text-nowrap">
+        <div className="w-[32px] px-1 flex justify-start items-center text-nowrap">
           {topLeftElement}
         </div>
         <div className="px-1 w-full flex justify-center items-center text-center">{title}</div>
-        <div className="w-[32px] px-1 sm:w-full flex justify-end items-center text-nowrap">
+        <div className="w-[32px] px-1 flex justify-end items-center text-nowrap">
           {topRightElement}
         </div>
       </div>
