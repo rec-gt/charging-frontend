6a2e950a6164f725ed6533ca9fec6d1188c9bf31
kychiu <kychiu@polyu.edu.hk>
2025-06-27 17:26:15 +0800

    feat: update

--- src/components/Plates/TextPlate.tsx
+++ src/components/Plates/TextPlate.tsx
@@ -4,14 +4,20 @@ import { Plate } from "./Plate";
 type TextPlateProps = {
   title: string;
   text: string;
+  topLeftElement?: any;
   icon?: any;
   isAlert: boolean;
 } & any;
 
 export const TextPlate: React.FC<TextPlateProps> = (props) => {
-  const { title, text, icon, isAlert, ...rest } = props;
+  const { title, text, topLeftElement, icon, isAlert, ...rest } = props;
   return (
-    <Plate title={title} topRightElement={icon} {...rest}>
+    <Plate
+      title={title}
+      topLeftElement={topLeftElement}
+      topRightElement={icon}
+      {...rest}
+    >
       <div
         className={`flex justify-center items-center h-full text-[clamp(15pt,3cqw,26pt)]! pb-2 ${
           isAlert ? "text-[#ff0000]" : "text-[#000]"
