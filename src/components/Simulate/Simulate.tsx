c272894ab6fe111bb27f0811ee4b05dedd0fa030
kychiu <kychiu@polyu.edu.hk>
2025-07-02 08:50:13 +0800

    feat: update

--- src/components/Simulate/Simulate.tsx
+++ src/components/Simulate/Simulate.tsx
@@ -7,33 +7,81 @@ import { setPageLoading } from "../../state/pageLoadingSlice";
 import { PageLayout } from "../PageLayout";
 import { SimulateTitle } from "../Title";
 
-const temperatureMarks = [
-  {
-    value: 0,
-    label: "0°C",
-  },
-  {
-    value: 20,
-    label: "20°C",
-  },
-  {
-    value: 40,
-    label: "40°C",
-  },
-  {
-    value: 60,
-    label: <div className="text-[#ff0000]">60°C⚠️</div>,
-  },
-  {
-    value: 80,
-    label: <div className="text-[#ff0000]">80°C⚠️</div>,
-  },
-];
-
 export const SimulatePage: React.FC = () => {
   const dispatch = useDispatch();
 
   const [simulation, setSimulation] = useState(false);
+  const [marks, setMarks] = useState({
+    AT_MARKS: [
+      { value: 0, label: "FOR_SET_POINT" },
+      {
+        value: 0,
+        label: "0°C",
+      },
+      {
+        value: 20,
+        label: "20°C",
+      },
+      {
+        value: 40,
+        label: "40°C",
+      },
+      {
+        value: 60,
+        label: "60°C",
+      },
+      {
+        value: 80,
+        label: "80°C",
+      },
+    ],
+    ST_MARKS: [
+      { value: 0, label: "FOR_SET_POINT" },
+      {
+        value: 0,
+        label: "0°C",
+      },
+      {
+        value: 20,
+        label: "20°C",
+      },
+      {
+        value: 40,
+        label: "40°C",
+      },
+      {
+        value: 60,
+        label: "60°C",
+      },
+      {
+        value: 80,
+        label: "80°C",
+      },
+    ],
+    A_MARKS: [
+      { value: 0, label: "FOR_SET_POINT" },
+      {
+        value: 0,
+        label: "0A",
+      },
+      {
+        value: 2,
+        label: "2A",
+      },
+      {
+        value: 4,
+        label: "4A",
+      },
+      {
+        value: 6,
+        label: "6A",
+      },
+      {
+        value: 8,
+        label: "8A",
+      },
+    ],
+  });
 
   const handleGetStats = async () => {
     await axios({
@@ -41,7 +89,36 @@ export const SimulatePage: React.FC = () => {
       url: `${backendServer}/system/get/stats`,
     })
       .then((res) => {
+        console.log(res.data);
+
         setSimulation(res.data.M == 3);
+        setMarks((prev) => {
+          prev.AT_MARKS[0] = {
+            value: res.data.SPT,
+            label: (
+              <div className="text-[#ff0000]">
+                {res.data.SPT}°C (THRESHOLD) ⚠️
+              </div>
+            ) as any,
+          };
+          prev.ST_MARKS[0] = {
+            value: res.data.SPT,
+            label: (
+              <div className="text-[#ff0000]">
+                {res.data.SPT}°C (THRESHOLD) ⚠️
+              </div>
+            ) as any,
+          };
+          prev.A_MARKS[0] = {
+            value: res.data.SPA,
+            label: (
+              <div className="text-[#ff0000]">
+                {res.data.SPA}A (THRESHOLD)⚠️
+              </div>
+            ) as any,
+          };
+          return prev;
+        });
       })
       .catch((err) => {
         console.log(err);
@@ -66,7 +143,7 @@ export const SimulatePage: React.FC = () => {
 
   useEffect(() => {
     dispatch(setPageLoading(true));
-    
+
     const interval = setInterval(() => {
       handleGetStats();
     }, 1000);
@@ -112,7 +189,7 @@ export const SimulatePage: React.FC = () => {
               step={1}
               valueLabelDisplay="on"
               orientation="vertical"
-              marks={temperatureMarks}
+              marks={marks.AT_MARKS}
               onChange={(_, value) => {
                 console.log(value);
               }}
@@ -131,7 +208,7 @@ export const SimulatePage: React.FC = () => {
               step={1}
               valueLabelDisplay="on"
               orientation="vertical"
-              marks={temperatureMarks}
+              marks={marks.ST_MARKS}
               onChange={(_, value) => {
                 console.log(value);
               }}
@@ -143,14 +220,14 @@ export const SimulatePage: React.FC = () => {
           <div className="h-full p-4">
             <Slider
               min={0}
-              max={80}
-              defaultValue={20}
+              max={8}
+              defaultValue={1}
               getAriaLabel={() => "Temperature"}
               getAriaValueText={(v) => `${v}°C`}
-              step={1}
+              step={0.1}
               valueLabelDisplay="on"
               orientation="vertical"
-              marks={temperatureMarks}
+              marks={marks.A_MARKS}
               onChange={(_, value) => {
                 console.log(value);
               }}
