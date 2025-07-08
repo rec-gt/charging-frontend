adaa97fe00202be7cb82bf2dc00d511d07d99c99
kychiu <kychiu@polyu.edu.hk>
2025-07-08 12:30:57 +0800

    feat: update

--- src/components/Header/Footer.tsx
+++ src/components/Header/Footer.tsx
@@ -2,8 +2,16 @@ import React from "react";
 
 export const Footer: React.FC = () => {
   return (
-    <div className="flex justify-center items-center w-full h-24 bg-(--color-primary) text-[#eee]">
+    <div className="fixed bottom-0 flex justify-center items-center w-full h-12 bg-(--color-primary) text-[#eee]">
       RGT © 2025 ALL RIGHTS RESERVED
+      <a
+        className="text-(--color-primary)"
+        target="_blank"
+        href="/admin/setting"
+      >
+        {" "}
+        S
+      </a>
     </div>
   );
 };
